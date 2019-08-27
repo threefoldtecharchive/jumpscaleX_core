726d4627a1362e98730901f18648bf8ccdbff7aa
serboctor <boctor.sarah@gmail.com>
2019-08-27 17:29:42 +0200

    Use get instead of new in zdbservers default start

--- JumpscaleCore/servers/zdb/ZDBServers.py
+++ JumpscaleCore/servers/zdb/ZDBServers.py
@@ -18,7 +18,7 @@ class ZDBServers(JSConfigs):
     @property
     def default(self):
         if not self._default:
-            self._default = self.new(name="default")
+            self._default = self.get(name="default")
         return self._default
 
     def build(self, reset=True):
