d6eb547d1412a93f8a4d80bc2010288a7dee3e58
Ahmed T. Youssef <xmonader@gmail.com>
2019-09-12 18:04:25 +0200

    gedis example

--- JumpscaleCore/servers/gedis_http/GedisHTTPFactory.py
+++ JumpscaleCore/servers/gedis_http/GedisHTTPFactory.py
@@ -3,7 +3,19 @@ from Jumpscale import j
 from bottle import post, run, response, request, Bottle
 
 app = Bottle()
+"""
+~> curl -i -XPOST localhost:9201/actors/blog/get_metadata --data '{"args":{"blog":"xmon"}}' -H "Content-Type: application/json"
+HTTP/1.1 200 OK
+Content-Type: application/json
+Content-Length: 376
+Date: Thu, 12 Sep 2019 16:01:13 GMT
 
+{"blog_name": "xmon", "blog_title": "xmonader weblog", "blog_description": "let there be posts", "author_name": "ahmed", "author_email": "ahmed@there.com", "author_image_filename": "", "base_url": "", "url": "", "posts_dir": "/sandbox/code/gitlab/xmonader/sample-blog-jsx/posts", "github_username": "xmonader", "github_repo_url": "git@gitlab.com:xmonader/sample-blog-jsx.git"}3BOTDEVEL:3bot:~: 
+
+~> curl -XPOST localhost:9201/actors/blog/get_tags
+["python", "lame", "markdown", "java"]3BOTDEVEL:3bot:~: 
+
+"""
 @app.route("/actors/<name>/<cmd>", method="post")
 def client_handler(name, cmd):
     client = j.clients.gedis.get(name='main_gedis_threebot', port=8901)
