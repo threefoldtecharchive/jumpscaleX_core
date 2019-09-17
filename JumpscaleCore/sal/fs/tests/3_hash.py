4f5e3f5471b1fe6ccadb644f3214074a44e216e4
Benjamin <benjamin.mateo@protonmail.com>
2019-09-17 10:10:22 +0200

    revert hash and add a tst fixes #91

--- JumpscaleCore/sal/fs/tests/3_hash.py
+++ JumpscaleCore/sal/fs/tests/3_hash.py
@@ -9,6 +9,7 @@ def main(self):
     j.tools.logger.debug = True
 
     test_path = "/tmp/test_dir/"
+    moved_path = "~/tmp2/another_test_dir/"
     file_path = "test.txt"
     file2_path = "test2.txt"
     file_content = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
@@ -16,6 +17,8 @@ def main(self):
     print("TEST getFolderMD5sum")
     if j.sal.fs.exists(test_path):
         j.sal.fs.remove(test_path)
+    if j.sal.fs.exists(moved_path):
+        j.sal.fs.remove(moved_path)
 
     j.sal.fs.createDir(test_path)
 
@@ -30,6 +33,16 @@ def main(self):
     assert hash_dir != hash_file
     assert hash_file == "af270cdfadd7512a60c7558dd0458db0"
     assert hash_dir == "8610d1c0268528b50e0a1099df3d9d74"
+
+    # let's move the directory to another place the hash should be the same
+    j.sal.fs.moveDir(test_path, moved_path)
+    assert j.sal.fs.exists(moved_path) == True
+    assert j.sal.fs.isFile(moved_path + file_path) == True
+    hash_moved_file = j.sal.fs.md5sum(moved_path + file_path)
+    hash_moved_dir = j.sal.fs.getFolderMD5sum(moved_path)
+    assert hash_dir == hash_moved_dir
+    assert hash_file == hash_moved_file
+    j.sal.fs.moveDir(moved_path, test_path)
 
     # let's add another file with same content
     j.sal.fs.writeFile(test_path + file2_path, file_content)
