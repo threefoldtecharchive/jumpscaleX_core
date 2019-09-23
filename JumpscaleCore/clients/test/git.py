3f37c45eb6e2e74e88371fb2472999609142a531
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2019-09-23 12:25:47 +0200

    git client automation testing

---
+++ JumpscaleCore/clients/test/git.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+from Jumpscale import j
+
+
+## currentDirGitRepo 
+
+class Gitclient(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+    
+    @classmethod
+    def tearDownClass(cls):
+        pass()
+
+    def test001_currentDirGitRepo(self):
+        """TC340
+        Test case for currentDirGitRepo option in git client 
+
+        **Test scenario**
+        #. cd to /sandbox/code/github/threefoldtech/jumpscaleX_core/
+        #. check 
+
+        """
+
+# os.chdir('/sandbox/code/github/threefoldtech/jumpscaleX_core/')
+# gitclient = j.clients.git.currentDirGitRepo()
+# gitclient.save()
+
+
