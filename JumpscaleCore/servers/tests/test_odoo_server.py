b9d92998e1b745af82d7c9edbba8fef1736363b0
dinaamagdy <magdyd@codescalers.com>
2019-09-29 10:53:02 +0200

    edit odoo

--- JumpscaleCore/servers/tests/test_odoo_server.py
+++ JumpscaleCore/servers/tests/test_odoo_server.py
@@ -5,7 +5,14 @@ import random, requests, uuid, unittest
 
 class TestOdooServer(BaseTest):
     def setUp(self):
-        pass
+        self.info("​Install odoo server , and get new instance of it ")
+        j.servers.odoo.install()
+        self.odoo_server = j.servers.odoo.get()
+        self.odoo_server.start()
+
+    def Teardown(self):
+        self.info(" Stop odoo server.")
+        self.odoo_server.stop()
 
     @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/92")
     def test01_create_database(self):
@@ -14,30 +21,26 @@ class TestOdooServer(BaseTest):
         - Create new database.
         - Check that created database exist in databases_list.
         - Check that new data base created successfully.
-        - Teardown odoo server.
+        - stop odoo server.
         """
-        self.info("​Install odoo server , and get new instance of it ")
-        j.servers.odoo.install()
-        odoo_server = j.servers.odoo.get()
-        odoo_server.start()
 
         self.info("Create new database ")
-        database = odoo_server.databases.new()
+        database = self.odoo_server.databases.new()
         self.set_database_data(database)
-        odoo_server.databases_create()
-        odoo_server.save()
+        self.odoo_server.databases_create()
+        self.odoo_server.save()
 
         self.info("Check that created database exist in databases_list.")
-        databases = odoo_server.databases_list()
+        databases = self.odoo_server.databases_list()
         self.assertIn(database.name, databases)
 
         self.info("Check that new database created successfully.")
-        database_client = odoo_server.get_client(database.name)
-        user_name = str(uuid.uuid4()).replace("-", "")[1:10]
-        user_password = str(uuid.uuid4()).replace("-", "")[1:10]
+        database_client = self.odoo_server.get_client(database.name)
+        user_name = self.rand_string()
+        user_password = self.rand_string()
         database_client.user_add(user_name, user_password)
         database_client.login(user_name, user_password)
-        wrong_passsword = str(uuid.uuid4()).replace("-", "")[1:10]
+        wrong_passsword = self.rand_string()
         with self.assertRaises(Exception):
             database_client.login(user_name, wrong_passsword)
 
@@ -45,8 +48,8 @@ class TestOdooServer(BaseTest):
         with self.assertRaises(Exception):
             database_client.login(user_name, user_password)
 
-        self.info(" Teardown odoo server.")
-        odoo_server.stop()
+        self.info(" stop odoo server.")
+        self.odoo_server.stop()
 
     @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/92")
     def test02_create_databases(self):
@@ -56,38 +59,30 @@ class TestOdooServer(BaseTest):
         - Create second database [db2] with reset=false, should create another database only..
         - Create another database [db3] with reset =true, should delete all old databases and create another one.
         """
-        self.info("​Install odoo server , and get new instance of it ")
-        j.servers.odoo.install()
-        odoo_server = j.servers.odoo.get()
-        odoo_server.start()
-
         self.info("Create database [db1].")
-        db1 = odoo_server.databases.new()
+        db1 = self.odoo_server.databases.new()
         self.set_database_data(db1)
-        odoo_server.databases_create()
-        odoo_server.save()
+        self.odoo_server.databases_create()
+        self.odoo_server.save()
 
         self.info("Create second database [db2] with reset=false, should create another database only.")
-        db2 = odoo_server.databases.new()
+        db2 = self.odoo_server.databases.new()
         self.set_database_data(db2)
-        odoo_server.databases_create(reset=False)
-        odoo_server.save()
-        self.assertIn(db1.name, odoo_server.databases_list())
-        self.assertIn(db2.name, odoo_server.databases_list())
+        self.odoo_server.databases_create(reset=False)
+        self.odoo_server.save()
+        self.assertIn(db1.name, self.odoo_server.databases_list())
+        self.assertIn(db2.name, self.odoo_server.databases_list())
 
         self.info(
             "Create another database [db3] with reset =true, should delete all old databases and create another one."
         )
-        db3 = odoo_server.databases.new()
+        db3 = self.odoo_server.databases.new()
         self.set_database_data(db3)
-        odoo_server.databases_create(reset=True)
-        odoo_server.save()
-        self.assertNotIn(db1.name, odoo_server.databases_list())
-        self.assertNotIn(db2.name, odoo_server.databases_list())
-        self.assertIn(db3.name, odoo_server.databases_list())
-
-        self.info(" Teardown odoo server.")
-        odoo_server.stop()
+        self.odoo_server.databases_create(reset=True)
+        self.odoo_server.save()
+        self.assertNotIn(db1.name, self.odoo_server.databases_list())
+        self.assertNotIn(db2.name, self.odoo_server.databases_list())
+        self.assertIn(db3.name, self.odoo_server.databases_list())
 
     @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/92")
     def test03_reset_databases(self):
@@ -95,24 +90,15 @@ class TestOdooServer(BaseTest):
         - ​Install and start odoo server , and get new instance of it . 
         - Try reset_database, should delete all databases.
         """
-
-        self.info("​Install odoo server , and get new instance of it ")
-        j.servers.odoo.install()
-        odoo_server = j.servers.odoo.get()
-        odoo_server.start()
-
         self.info("Create database.")
-        db = odoo_server.databases.new()
+        db = self.odoo_server.databases.new()
         self.set_database_data(db)
-        odoo_server.databases_create()
-        odoo_server.save()
+        self.odoo_server.databases_create()
+        self.odoo_server.save()
 
         self.info("Try reset_database, should delete all databases.")
-        odoo_server.databases_reset()
-        self.assertFalse(odoo_server.databases_list())
-
-        self.info(" Teardown odoo server.")
-        odoo_server.stop()
+        self.odoo_server.databases_reset()
+        self.assertFalse(self.odoo_server.databases_list())
 
     @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/92")
     def test04_export_import_databases(self):
@@ -121,40 +107,34 @@ class TestOdooServer(BaseTest):
         - Export created database, check that zip file exist.
         - Import database, check that imported database exist in database list
         """
-
-        self.info("​Install odoo server , and get new instance of it ")
-        j.servers.odoo.install()
-        odoo_server = j.servers.odoo.get()
-        odoo_server.start()
-
         self.info("Create database.")
-        db = odoo_server.databases.new()
+        db = self.odoo_server.databases.new()
         self.set_database_data(db)
-        odoo_server.databases_create()
-        odoo_server.save()
+        self.odoo_server.databases_create()
+        self.odoo_server.save()
 
         self.info("Export created database, check that zip file exist.")
         export_dir = "/root/exports"
-        export_dest = self.os_command("mkdir {}".format(export_dir))
-        odoo_server.database_export(db.name, export_dir)
+        output, error = self.os_command("mkdir {}".format(export_dir))
+        self.odoo_server.database_export(db.name, export_dir)
         output, error = self.os_command(" ls /root/exports")
         self.assertIn("{}.zip".format(db.name), output.decode())
 
         self.info("Import database, check that imported database exist in database list")
-        odoo_server.databases_reset()
+        self.odoo_server.databases_reset()
         database_name = str(uuid.uuid4()).replace("-", "")[1:10]
-        odoo_server.database_import(database_name, export_dir)
-        self.assertIn(database_name, odoo_server.databases_list())
+        self.odoo_server.database_import(database_name, export_dir)
+        self.assertIn(database_name, self.odoo_server.databases_list())
 
-        self.info(" Teardown odoo server.")
-        odoo_server.stop()
-
-    # def test05_write_and_read(self):
-    #     """
-    #     - ​Install and start odoo server , and get new instance of it .
-    #     - Create database [db].
-    #     - Wrtie data[dt] in [db], check that it writes successfully.
-    #     - Export data [dt].
-    #     - Import data [dt].
-    #     - Read data [dt] from db [db].
-    #     - Delete data [dt], check it deleted successfully.
+    @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/92")
+    def test05_write_and_read(self):
+        """
+        - ​Install and start odoo server , and get new instance of it .
+        - Create database [db].
+        - Wrtie data[dt] in [db], check that it writes successfully.
+        - Export data [dt].
+        - Import data [dt].
+        - Read data [dt] from db [db].
+        - Delete data [dt], check it deleted successfully.
+        """
+        pass
