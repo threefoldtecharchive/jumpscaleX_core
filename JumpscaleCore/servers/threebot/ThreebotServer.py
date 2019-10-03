497faf3c7e68bfe21542598dbd7c8e1248266749
andrewayoub <andrew.a.mikhaeel@gmail.com>
2019-10-03 17:22:19 +0200

    wait for gedis to start before getting the client

--- JumpscaleCore/servers/threebot/ThreebotServer.py
+++ JumpscaleCore/servers/threebot/ThreebotServer.py
@@ -155,8 +155,13 @@ class ThreeBotServer(j.baseclasses.object_config):
             if not cmd.is_running():
                 cmd.start()
                 time.sleep(2)
-            r = j.sal.nettools.tcpPortConnectionTest("127.0.0.1", 8901, timeout=120)
 
+        # FIXME: wait for the connection properly
+        trials = 0
+        while not j.sal.nettools.tcpPortConnectionTest("127.0.0.1", 8901, timeout=120) and trials < 20:
+            gevent.sleep(1)
+            trials += 1
+            
         self.client = j.clients.gedis.get(name="threebot", port=8901, namespace="default")
         # TODO: will have to authenticate myself
 
