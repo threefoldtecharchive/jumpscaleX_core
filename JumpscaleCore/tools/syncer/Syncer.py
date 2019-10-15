5d55ac8c1a144e8cee326bc92b067e81e5c49f13
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-10-15 14:10:51 +0200

    Fixing issues related to Syncer, Fixing deletion and add move action handler

--- JumpscaleCore/tools/syncer/Syncer.py
+++ JumpscaleCore/tools/syncer/Syncer.py
@@ -4,9 +4,12 @@ from watchdog.observers import Observer
 from .MyFileSystemEventHandler import MyFileSystemEventHandler
 import gevent
 from gevent import time
+import traceback
+import re
 
 
 class Syncer(j.baseclasses.object_config):
+
     """
     make sure there is an ssh client first, can be done by
 
@@ -139,14 +142,31 @@ class Syncer(j.baseclasses.object_config):
         self._monitor.start()
         print("monitor started, are out of loop")
 
-        j.shell()
+        # j.shell()
         time.sleep(3600)
 
+    def ignore_file(self, file_name):
+        file_extension = j.sal.fs.getFileExtension(file_name)
+        if file_extension is "":
+            return True
+        result = file_name.split(".")
+        if result.count(file_extension) > 1:
+            return True
+        return False
+
     def handler(self, event, action="copy"):
+        # TODO Remove logging and log file after fixing all the issues
+        path = "/sandbox/code/github/threefoldtech/jumpscaleX_core/JumpscaleCore/tools/log.txt"
+        logs_file = open(path, "a")
+        logs_file.write("testing the logging\n")
         self._log_info("......................new event........................")
+        logs_file.write("......................new event........................\n")
         self._log_info("syncer handle")
+        logs_file.write("SYNCER hanlder\n")
         self._log_info("event:%s" % event)
+        logs_file.write("event:%s \n" % event)
         self._log_info("action:%s" % action)
+        logs_file.write("action:%s \n" % action)
 
         self._log_debug("%s:%s" % (event, action))
         for key, sshclient in self.sshclients.items():
@@ -181,22 +201,40 @@ class Syncer(j.baseclasses.object_config):
                     return
                 elif changedfile.endswith("___"):
                     return
+                elif changedfile.endswith("~"):
+                    return
+                elif hasattr(event, "dest_path"):
+                    if event.dest_path.endswith("~"):
+                        return
                 dest = self._path_dest_get(executor=sshclient.executor, src=changedfile)
                 self._log_info("destination file name: %s" % dest)
 
                 e = ""
                 self._log_debug("action:%s for %s" % (action, changedfile))
 
+                # handle ignored file
+                is_ignored = self.ignore_file(changedfile)
+
                 rc = 1
                 counter = 0
                 while rc == 1 and counter < 10:
                     counter += 1
                     if action == "copy":
+                        logs_file.write(
+                            "copy action will excute: (ssh:%s): %s: %s \n" % (sshclient.name, changedfile, dest)
+                        )
                         self._log_info("copy (ssh:%s): %s:%s" % (sshclient.name, changedfile, dest))
                         try:
+                            # TODO remove the tracing issue
+                            # regex = " *.py.*.py"
+                            # regexFound = re.findall(regex, changedfile)
+                            # if regexFound:
+                            #     traceback.print_stack(file=logs_file)
+
                             sshclient.file_copy(changedfile, dest)
                             rc = 0
                             self._log_info("OK")
+                            logs_file.write("copy done successfully \n")
                         except Exception as e:
                             j.shell()
                             if str(e).find("SocketSendError") != -1:
@@ -206,16 +244,20 @@ class Syncer(j.baseclasses.object_config):
                                 rc = 2
                                 break
                     elif action == "delete":
+                        logs_file.write("delete action will excute : %s:%s \n" % (changedfile, dest))
                         self._log_debug("delete: %s:%s" % (changedfile, dest))
-                        if self.ignore_delete:
+                        if is_ignored:
                             return
                         try:
                             cmd = "rm %s" % dest
-                            sshclient.exec_command(cmd)
+                            logs_file.write("The CMD command : %s \n" % (cmd))
+                            sshclient.execute(cmd)
                             rc = 0
                             self._log_info("OK")
+                            logs_file.write("deletion done successfully \n")
                         except Exception as e:
                             self._log_error("Couldn't remove file: %s" % (dest))
+                            logs_file.write("Couldn't remove file: %s \n" % (dest))
                             if "No such file" in str(e):
                                 rc = 0
                                 continue
@@ -223,13 +265,33 @@ class Syncer(j.baseclasses.object_config):
                                 rc = 1
                                 continue
                     elif action == "moved":
-                        self._log_debug("moving event : %s:%s" % (changedfile, dest))
+                        logs_file.write("moved action will excute: %s:%s \n" % (changedfile, event.dest_path))
+                        self._log_debug("moving  : %s:%s" % (changedfile, event.dest_path))
+                        try:
+                            cmd = "mv  %s %s" % (changedfile, event.dest_path)
+                            logs_file.write("The CMD command that will excute: %s \n" % (cmd))
+                            sshclient.execute(cmd)
+                            rc = 0
+                            self._log_info("OK")
+                            logs_file.write("move done successfully \n")
+                        except Exception as e:
+                            self._log_error("Couldn't move file: %s" % (event.dest_path))
+                            logs_file.write("couldn't move file bec.  : %s \n" % (e))
+                            if "No such file" in str(e):
+                                rc = 0
+                                continue
+                            else:
+                                rc = 1
+                                continue
                     else:
                         raise j.exceptions.JSBUG("action not understood in filesystemhandler on sync:%s" % action)
 
                 if rc > 0:
                     self._log_error("Couldn't sync file: %s:%s" % (changedfile, dest))
+                    logs_file.write("Couldn't sync file: %s:%s \n" % (changedfile, dest))
+                    logs_file.write("There is an ERROR WiLL SYNC ALL \n")
                     self._log_error("** ERROR IN COPY, WILL SYNC ALL")
+                    logs_file.close()
                     try:
                         self._log_error(str(e))
                     except:
