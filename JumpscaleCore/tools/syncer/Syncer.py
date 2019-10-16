1478e9a28920fb63cfd0a099ee0b041d75edfe81
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-10-16 09:51:37 +0200

    Syncer tool code cleaning

--- JumpscaleCore/tools/syncer/Syncer.py
+++ JumpscaleCore/tools/syncer/Syncer.py
@@ -134,40 +134,23 @@ class Syncer(j.baseclasses.object_config):
         from .MyFileSystemEventHandler import FileSystemMonitor
 
         self._monitor = FileSystemMonitor(syncer=self)
-        # if j.servers.rack.current:
-        #     j.servers.rack.current.greenlets["fs_sync_monitor"] = self.monitor_greenlet
-
         print("the only way how it works today, don't ask me why:")
         print("if your ssh connection is called 'master'")
         print("kosmos -p")
         print("then in shell do: 'j.clients.ssh.master.syncer.sync()'")
         self._monitor.start()
         print("monitor started, are out of loop")
-
-        # j.shell()
         time.sleep(3600)
 
     def handler(self, event, action="copy"):
-        # TODO Remove logging and log file after fixing all the issues
-        path = "/sandbox/code/github/threefoldtech/jumpscaleX_core/JumpscaleCore/tools/log.txt"
-        logs_file = open(path, "a")
-        logs_file.write("testing the logging\n")
         self._log_info("......................new event........................")
-        logs_file.write("......................new event........................\n")
         self._log_info("syncer handle")
-        logs_file.write("SYNCER hanlder\n")
         self._log_info("event:%s" % event)
-        logs_file.write("event:%s \n" % event)
         self._log_info("action:%s" % action)
-        logs_file.write("action:%s \n" % action)
-
         self._log_debug("%s:%s" % (event, action))
         for key, sshclient in self.sshclients.items():
             if sshclient.executor.isContainer:
                 continue
-            # self._log_debug("open sftp to sshclient '%s'" % key)
-            # # ftp = sshclient.sftp
-            # j.shell()
             changedfile = event.src_path
             if event.src_path.endswith((".swp", ".swx")):
                 return
@@ -210,21 +193,11 @@ class Syncer(j.baseclasses.object_config):
                 while rc == 1 and counter < 10:
                     counter += 1
                     if action == "copy":
-                        logs_file.write(
-                            "copy action will excute: (ssh:%s): %s: %s \n" % (sshclient.name, changedfile, dest)
-                        )
                         self._log_info("copy (ssh:%s): %s:%s" % (sshclient.name, changedfile, dest))
                         try:
-                            # TODO remove the tracing issue
-                            # regex = " *.py.*.py"
-                            # regexFound = re.findall(regex, changedfile)
-                            # if regexFound:
-                            #     traceback.print_stack(file=logs_file)
-
                             sshclient.file_copy(changedfile, dest)
                             rc = 0
                             self._log_info("OK")
-                            logs_file.write("copy done successfully \n")
                         except Exception as e:
                             j.shell()
                             if str(e).find("SocketSendError") != -1:
@@ -234,19 +207,14 @@ class Syncer(j.baseclasses.object_config):
                                 rc = 2
                                 break
                     elif action == "delete":
-                        logs_file.write("delete action will excute : %s:%s \n" % (changedfile, dest))
                         self._log_debug("delete: %s:%s" % (changedfile, dest))
-
                         try:
                             cmd = "rm -f %s" % dest
-                            logs_file.write("The CMD command : %s \n" % (cmd))
                             sshclient.execute(cmd)
                             rc = 0
                             self._log_info("OK")
-                            logs_file.write("deletion done successfully \n")
                         except Exception as e:
                             self._log_error("Couldn't remove file: %s" % (dest))
-                            logs_file.write("Couldn't remove file: %s \n" % (dest))
                             if "No such file" in str(e):
                                 rc = 0
                                 continue
@@ -254,18 +222,14 @@ class Syncer(j.baseclasses.object_config):
                                 rc = 1
                                 continue
                     elif action == "moved":
-                        logs_file.write("moved action will excute: %s:%s \n" % (changedfile, event.dest_path))
                         self._log_debug("moving  : %s:%s" % (changedfile, event.dest_path))
                         try:
                             cmd = "mv  %s %s" % (changedfile, event.dest_path)
-                            logs_file.write("The CMD command that will excute: %s \n" % (cmd))
                             sshclient.execute(cmd)
                             rc = 0
                             self._log_info("OK")
-                            logs_file.write("move done successfully \n")
                         except Exception as e:
                             self._log_error("Couldn't move file: %s" % (event.dest_path))
-                            logs_file.write("couldn't move file bec.  : %s \n" % (e))
                             if "No such file" in str(e):
                                 rc = 0
                                 continue
@@ -277,10 +241,7 @@ class Syncer(j.baseclasses.object_config):
 
                 if rc > 0:
                     self._log_error("Couldn't sync file: %s:%s" % (changedfile, dest))
-                    logs_file.write("Couldn't sync file: %s:%s \n" % (changedfile, dest))
-                    logs_file.write("There is an ERROR WiLL SYNC ALL \n")
-                    self._log_error("** ERROR IN COPY, WILL SYNC ALL")
-                    logs_file.close()
+                    self._log_error("** ERROR HAPPENED, WILL SYNC ALL")
                     try:
                         self._log_error(str(e))
                     except:
