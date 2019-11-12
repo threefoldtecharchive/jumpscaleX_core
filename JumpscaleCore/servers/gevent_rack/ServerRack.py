63f14bf333de24a7805517f143e5c51a3ffb3eac
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2019-11-12 13:28:23 +0200

    Update serverRack.py to reuse the enable_cors from GedisHTTPFactory.py

--- JumpscaleCore/servers/gevent_rack/ServerRack.py
+++ JumpscaleCore/servers/gevent_rack/ServerRack.py
@@ -1,4 +1,5 @@
 from Jumpscale import j
+from Jumpscale.servers.gedis_http.GedisHTTPFactory import enable_cors
 
 import sys
 import mimetypes
@@ -81,22 +82,6 @@ class ServerRack(JSBASE):
         if not app:
 
             from bottle import route, template, request, Bottle, abort, template, response
-
-            # the decorator
-            def enable_cors(fn):
-                def _enable_cors(*args, **kwargs):
-                    # set CORS headers
-                    response.headers["Access-Control-Allow-Origin"] = "*"
-                    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS"
-                    response.headers[
-                        "Access-Control-Allow-Headers"
-                    ] = "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
-
-                    if request.method != "OPTIONS":
-                        # actual request; reply with the actual response
-                        return fn(*args, **kwargs)
-
-                return _enable_cors
 
             app = Bottle()
 
