82816472164c77fcf6d1893c3ba0e6e98cf4813d
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-11-12 15:36:17 +0200

    add error handling to gdrive actor

--- JumpscaleCore/servers/threebot/base_actors/gdrive.py
+++ JumpscaleCore/servers/threebot/base_actors/gdrive.py
@@ -1,4 +1,6 @@
 from Jumpscale import j
+from googleapiclient.errors import HttpError as GoogleApiHTTPError
+
 
 STATIC_DIR = "/sandbox/var/gdrive/static"
 
@@ -12,7 +14,9 @@ class gdrive(j.baseclasses.threebot_actor):
         guid2 = "" (S)
         ```
         ```out
-        res = (S)
+        res = "" (S)
+        error_message = "" (S)
+        error_code = 0 (I)
         ```
         :param collection:
         :param bucket:
@@ -26,23 +30,30 @@ class gdrive(j.baseclasses.threebot_actor):
         out = schema_out.new()
 
         if not doctype in doctypes_map:
-            raise j.exceptions.Input("invalid type")
+            out.error_code = -1
+            allowed_types = ", ".join(doctypes_map.keys())
+            out.error_message = f"invalid document type of '{doctype}', allowed types are {allowed_types}."
+            return out
 
         service_name = doctypes_map[doctype]
-        if doctype in ["document", "spreadsheets", "presentation"]:
-            path = j.sal.fs.joinPaths(STATIC_DIR, doctype, "{}.pdf".format(guid1))
-            cl.exportFile(guid1, destpath=path, service_name=service_name, service_version="v3")
-
-            out.res = "/gdrive_static/{}/{}.pdf".format(doctype, guid1)
-        elif doctype == "slide":
-            path = j.sal.fs.joinPaths(STATIC_DIR, doctype)
-            cl.exportSlides(guid1, path)
-            if j.sal.fs.exists("{}/{}/{}.png".format(path, guid1, guid2), followlinks=True):
-                out.res = "/gdrive_static/slide/{}/{}.png".format(guid1, guid2)
-            else:
-                meta = cl.get_presentation_meta("{}/presentations.meta.json".format(path), guid1)
-                guid2 = meta[guid2]
-                guid2 = guid2.split("_", maxsplit=1)[1]  # remove the 0x_ part from the file name
-                out.res = "/gdrive_static/slide/{}/{}".format(guid1, guid2)
-
+        try:
+            if doctype in ["document", "spreadsheets", "presentation"]:
+                path = j.sal.fs.joinPaths(STATIC_DIR, doctype, "{}.pdf".format(guid1))
+                cl.exportFile(guid1, destpath=path, service_name=service_name, service_version="v3")
+                out.res = "/gdrive_static/{}/{}.pdf".format(doctype, guid1)
+            elif doctype == "slide":
+                path = j.sal.fs.joinPaths(STATIC_DIR, doctype)
+                cl.exportSlides(guid1, path)
+                if j.sal.fs.exists("{}/{}/{}.png".format(path, guid1, guid2), followlinks=True):
+                    out.res = "/gdrive_static/slide/{}/{}.png".format(guid1, guid2)
+                else:
+                    meta = cl.get_presentation_meta("{}/presentations.meta.json".format(path), guid1)
+                    if guid2 in meta:
+                        guid2 = meta[guid2]
+                        guid2 = guid2.split("_", maxsplit=1)[1]  # remove the 0x_ part from the file name
+                        out.res = "/gdrive_static/slide/{}/{}".format(guid1, guid2)
+        except GoogleApiHTTPError as api_http_error:
+            error = j.data.serializers.json.loads(api_http_error.content)["error"]
+            out.error_code = error["code"]
+            out.error_message = error["message"]
         return out
