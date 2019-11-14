77a06b43221c6f8cd327a46b4407a03252ade1fc
AhmedHanafy725 <41957921+AhmedHanafy725@users.noreply.github.com>
2019-11-14 14:28:05 +0200

    Development fixes (#228)
    * Add openresty and myjobs tests to run with every commit
    
    * Fix gedis tests
    
    * Increase threebot server timeout
    
    * Fix timeout in startupcmd
    
    *  makse schema tests run
    
    * jobs tests fixes
    
    * Formatting

--- JumpscaleCore/servers/gedis/pytests/test_simple_eco.py
+++ JumpscaleCore/servers/gedis/pytests/test_simple_eco.py
@@ -12,9 +12,9 @@ import redis
 class TestSimpleEcho(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.client = j.servers.threebot.local_start_default()
+        cls.client = j.servers.threebot.local_start_default(timeout=1200)
         package_path = j.sal.fs.joinPaths(j.sal.fs.getDirName(__file__), "test_package")
-        cls.client.actors.package_manager.package_add("test_package", path=package_path)
+        cls.client.actors.package_manager.package_add(path=package_path)
         cls.client.reload()
 
     @classmethod
