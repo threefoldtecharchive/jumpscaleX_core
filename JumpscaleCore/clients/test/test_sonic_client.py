a2953ff7e5b25eed302db095a81d4e411b2a50d8
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2019-11-18 18:24:08 +0200

    sonic client test cases

--- JumpscaleCore/clients/test/test_sonic_client.py
+++ JumpscaleCore/clients/test/test_sonic_client.py
@@ -221,7 +221,97 @@ class SonicClient(BaseTest):
         self.info("Use pop method to pop the latest index")
         self.assertEqual(self.client.pop("RANDOM_COLLECTION", "RANDOM_BUCKET", 'test', "test"), 0)
 
-    def test012_
+    def test012_flush_for_exists_collection_and_bucket(self):
+        """
+        TC 534
+        Test Case to test flush for exists collection and bucket.
+
+        **Test scenario**
+        #. Push the data to sonic server.
+        #. Use flush to flush certain collection with certain bucket.
+        #. Use count method to check that output, should equal to 0.
+        """
+        self.info("Push the data to sonic server")
+        for article_id, content in self.data.items():
+            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
+
+        self.info("Use flush to flush certain collection with certain bucket")
+        self.assertNotEquals(self.client.flush(self.COLLECTION, self.BUCKET), 0)
+
+        self.info("Use count method to check that output")
+        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 0)
+
+    def test013_flush_for_non_exists_collection_and_bucket(self):
+        """
+        TC 535
+        Test Case for flush method for non exists collection and method.
+
+        **Test scenario**
+        #. Use flush to flush non exists collection with certain bucket.
+        """
+        self.info("Use flush to flush non exists collection with certain bucket")
+        self.assertEquals(self.client.flush("RANDOM_COLLECTION", "RANDOM_BUCKET"), 0)
+
+    def test014_flush_object_method_for_exists_object_in_certain_collection_with_certain_bucket(self):
+        """
+        TC 536
+        Test Case for flush_object method for certain object in certain collection with certain bucket.
+
+        **Test scenario**
+        #. Push the data to sonic server.
+        #. Use flush_object to flush certain object in collection with certain bucket.
+        #. Use query to check that this record is flushed.
+        """
+        self.info("Push the data to sonic server")
+        for article_id, content in self.data.items():
+            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
+
+        self.info("Use flush_object to flush certain object in collection with certain bucket")
+        self.assertNotEqual(self.client.flush_object(self.COLLECTION, self.BUCKET, 'post:4'), 0)
+
+        self.info("Use query to check that this record is flushed")
+        self.assertEqual(self.client.query(self.COLLECTION, self.BUCKET, "love"), ['post:5'])
+
+    def test015_flush_object_method_for_non_exists_object_in_certain_collection_with_certain_bucket(self):
+        """
+        TC 537
+        Test Case for flush_object for non exists object.
+
+        **Test scenario**
+        #. Use flush_object to flush non exists object in collection with certain bucket.
+        """
+        self.info("Use flush_object to flush non exists object in collection with certain bucket")
+        self.assertEqual(self.client.flush_object("RANDOM_COLLECTION", "RANDOM_BUCKET", "RANDOM_OBJECT"), 0)
+
+    def test016_flush_bucket_for_exists_bucket_in_certain_collection(self):
+        """
+        TC 538
+        Test Case for flush_object method for certain bucket in certain collection.
+
+        **Test scenario**
+        #. Push the data to sonic server.
+        #. Push data to another bucket within the same collection.
+        #. Check that their is no more objects in the second created bucket.
+
+        """
+        self.info("Push the data to sonic server")
+        for article_id, content in self.data.items():
+            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
+
+        self.info("Push data to another bucket within the same collection")
+        self.assertTrue(self.client.push(self.COLLECTION, "posts_1", "test", "test for flush_bucket"))
+
+        self.info("Check that their is no more objects in the second created bucket")
+        self.assertEqual(self.client.count(self.COLLECTION, "posts_1"), 0)
+
+        self.info("Check that their are objects in the first created bucket")
+        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 6)
+
+
+
+
+
+
 
 
 
