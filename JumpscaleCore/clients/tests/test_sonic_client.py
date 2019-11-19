41fcab3b9281e7852832f81665743905d28d1753
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2019-11-19 16:55:10 +0200

    add some changes to sonic client

--- JumpscaleCore/clients/tests/test_sonic_client.py
+++ JumpscaleCore/clients/tests/test_sonic_client.py
@@ -1,6 +1,5 @@
 import unittest
 from Jumpscale import j
-from random import randint
 from base_test import BaseTest
 
 
@@ -21,297 +20,160 @@ class SonicClient(BaseTest):
         print('\t')
         self.info('Test case : {}'.format(self._testMethodName))
 
+        RAND_NUM = self.rand_num()
+        self.RAND_STRING_1 = self.rand_string(10)
+        self.RAND_STRING_2 = self.rand_string(15)
+
         self.data = {
-            'post:1': "this is some test text hello",
-            'post:2': 'this is a hello world post',
-            'post:3': "hello how is it going?",
-            'post:4': "for the love of god?",
-            'post:5': "for the love of a lord?",
+            'post:1': "this is some test {}, for love of JSX".format(self.RAND_STRING_1),
+            'post:2': 'this is a hello {}'.format(self.RAND_STRING_2),
+            'post:3': "hello {}?".format(self.RAND_STRING_1),
+            'post:4': "for the love of {}?".format(self.RAND_STRING_2),
+            'post:5': "for the lord of {}".format(RAND_NUM)
         }
 
-        self.COLLECTION = "COLL_{}".format(randint(1, 1000))
-        self.BUCKET = "BUCKET_{}".format(randint(1, 1000))
-
-    def tearDown(self):
-        self.info("Flush all data in {} collection, and {} bucket".format(self.COLLECTION, self.BUCKET))
-        self.client.flush(self.COLLECTION, self.BUCKET)
-
-    @classmethod
-    def TearDownClass(cls):
-        cls.info("Stop Sonic server")
-        j.servers.sonic.default.stop()
-
-        cls.info("Uninstall Sonic builder")
-        j.builders.apps.sonic.uninstall()
-
-    def test001_push(self):
-        """
-        TC 522
-        Test Case to test Push method in Sonic client, with valid collection, bucket, object, text.
-
-        **Test scenario**
-        #. Push the data to sonic server.
-        #. Check the count of indexed search data in the collection and bucket, should equal to 6.
-        """
+        self.COLLECTION = "COLL_{}".format(RAND_NUM)
+        self.BUCKET = "BUCKET_{}".format(RAND_NUM)
 
         self.info("push the data to sonic server")
         for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
+            self.client.push(self.COLLECTION, self.BUCKET, article_id, content)
+
+    def tearDown(self):
+        self.info("Flush all data in {} collection, and {} bucket".format(self.COLLECTION, self.BUCKET))
+        self.client.flush(self.COLLECTION)
+
+    def test001_push_with_collection_and_bucket(self):
+        """
+        TC 522
+        Test case to Push method in Sonic client, with valid collection, bucket, object, text.
+
+        **Test scenario**
+        #. Check the count of indexed search data in the collection and bucket, should equal to length of data.
+        """
 
         self.info("check the count of indexed search data in the collection and bucket")
-        self.assertEquals(self.client.count(self.COLLECTION, self.BUCKET), 6)
+        self.assertEquals(self.client.count(self.COLLECTION, self.BUCKET), len(self.data))
 
-    def test002_push_without_collection_or_bucket(self):
-        """
-        TC 523
-        Test Case to test push method without collection and bucket.
-
-        **Test scenario**
-        #. Test push method without collection and bucket.
-        """
-
-        self.info("Test push method without collection and bucket")
-        with self.assertRaises(Exception):
-            self.client.push('post_1', "test should fail")
-    #
-    # def test003_push_with_diff_collection_and_bucket(self):
-    #     """
-    #     TC 524
-    #     Test Case to push method with different collection and bucket.
-    #
-    #     **Test scenario**
-    #     #. Push the data to sonic server.
-    #     #. Check the count of indexed search data in the collection and bucket, should equal to 6.
-    #     #. Redo step 1 again, with different collection and bucket.
-    #     #. Check again the number of indexed data in the second collection and bucket, should equal to 2.
-    #     """
-    #
-    #     self.info("Push the data to sonic server")
-    #     for article_id, content in self.data.items():
-    #         self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
-    #
-    #     self.info("Check the count of indexed search data in the collection and bucket")
-    #     self.assertEquals(self.client.count(self.COLLECTION, self.BUCKET), 6)
-    #
-    #     self.info("Push the data to sonic server, with different collection and bucket name")
-    #     collection_new = "COLL_{}".format(randint(1, 1000))
-    #     bucket_new = "BUCKET_{}".format(randint(1, 1000))
-    #     self.client.push(collection_new, bucket_new, "post_1", "test push with different data")
-    #
-    #     self.info("Check the count of indexed search data in the new collection and bucket")
-    #     self.assertEquals(self.client.count(collection_new, bucket_new), 2)
-
-    def test004_query_with_exists_collection_and_bucket(self):
+    def test002_query_for_certain_collection_and_bucket(self):
         """
         TC 526
-        Test Case to test query with an exists collection and bucket name.
+        Test Case to query method with certain collection and data.
 
         **Test scenario**
-        #. Push the data to sonic server.
-        #. Query to those data, and check the output.
+        #. Query to certain data with valid collection and bucket name.
+        #. Query to certain data with non valid collection and bucket name.
         """
 
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
-
-        # self.assertEqual(len(self.client.query(self.COLLECTION, self.BUCKET, "love")), 2)
+        self.info("Query to certain data with valid collection and bucket name, and check the output.")
         self.assertEqual(
-            sorted(self.client.query(self.COLLECTION, self.BUCKET, "love")), sorted(['post:5', 'post:4'])
+            sorted(self.client.query(self.COLLECTION, self.BUCKET, self.RAND_STRING_1)), ['post:1', 'post:3']
         )
 
-    def test005_query_with_non_exists_collection_and_bucket(self):
-        """
-        TC 527
-        Test Case to test query with non exists collection and bucket.
+        # need to check the correct behaviour for this query.
+        self.info("Query for non valid collection and bucket")
+        self.assertEqual(len(self.client.query("RANDOM_COLLECTION", "RANDOM_BUCKET", "RANDOM_OBJECT")), 0)
 
-        **Test scenario**
-        #. Query for non exists collection and bucket.
-        """
-
-        self.info("Query for non exists collection and bucket.")
-        self.assertEqual(len(self.client.query("RANDOM_COLLECTION", "RANDOM_BUCKET", "test")), 0)
-
-    def test006_suggest_with_exists_collection_and_bucket(self):
+    def test003_suggest_with_certain_collection_and_bucket(self):
         """
         TC 528
-        Test Case to test suggest with an exists collection and bucket name.
+        Test Case to suggest with certain collection and bucket name.
 
         **Test scenario**
-        #. Push the data to sonic server.
-        #. Use suggest method and check the output.
+        #. Use suggest method with valid collection and bucket name.
+        #. Use suggest method with non valid collection and bucket name.
         """
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
 
-        self.info("Use suggest method and check the output")
-        # self.assertEquals(self.client.suggest(self.COLLECTION, self.BUCKET, "lo"), 2)
+        self.info("Use suggest method with valid collection and bucket name")
         self.assertEqual(
-            sorted(self.client.suggest(self.COLLECTION, self.BUCKET, "lo")), sorted(['lord', 'love'])
+            sorted(self.client.suggest(self.COLLECTION, self.BUCKET, "lo")), ['lord', 'love']
         )
 
-    def test007_suggest_with_non_exists_collection_and_bucket(self):
-        """
-        TC 529
-        Test Case to test with non exists collection and bucket name.
-
-        **Test scenario**
-        #. Use suggest method with non exists collection and bucket name, should return PENDING.
-        """
-        self.info("Use suggest method with non exists collection and bucket name")
+        # need to check the correct behaviour for this query.
+        self.info("Use suggest method with non valid collection and bucket name")
         self.assertIn('PENDING', self.client.suggest("RANDOM_COLLECTION", "RANDOM_COLLECTION", "RANDOM"))
 
-    # def test008_count_for_exists_collection_and_bucket(self):
-    #     """
-    #     TC 530
-    #     Test Case to test count method with exists collection and bucket name.
-    #
-    #     **Test scenario**
-    #     #. Push the data to sonic server.
-    #     #. Use count method with exists collection and bucket name, the output should be 6.
-    #     """
-    #     self.info("Push the data to sonic server")
-    #     for article_id, content in self.data.items():
-    #         self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
-    #
-    #     self.info("check the count of indexed search data in the collection and bucket")
-    #     self.assertEquals(self.client.count(self.COLLECTION, self.BUCKET), 6)
+    @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/272")
+    def test004_count_with_non_valid_collection_and_bucket(self):
+        """
+        TC 531
+        Test Case to count method with non valid collection and bucket name.
 
-    # def test009_count_for_exists_collection_and_bucket(self):
-    #     """
-    #     TC 531
-    #     Test Case to test count method with non exists collection and bucket name.
-    #
-    #     **Test scenario**
-    #     #. Use count method with exists collection and bucket name, the output should equal to 0.
-    #     """
-    #     self.info("Use count method with exists collection and bucket name, the output should equal to 0")
-    #     self.assertEqual(self.client.count("RANDOM_COLLECTION", "RANDOM_BUCKET"), 0)
+        **Test scenario**
+        #. Use count method with non valid collection and bucket name, the output should be 0.
+        """
+        self.info("Use count method non valid collection and bucket name, the output should equal to 0")
+        self.assertEqual(self.client.count("RANDOM_COLLECTION", "RANDOM_BUCKET"), 0)
 
-    def test010_pop_for_exists_collection_and_bucket(self):
+    @unittest.skip("https://github.com/threefoldtech/jumpscaleX_core/issues/272")
+    def test005_pop_with_certain_collection_and_bucket(self):
         """
         TC 532
-        Test Case to test pop method with exists collection and bucket.
+        Test Case to pop method with certain collection and bucket.
 
         **Test scenario**
-        #. Push the data to sonic server.
-        #. Use pop method to pop the latest index, the output shouldn't equal to 0.
-        #. Use count method to check that output, should equal to 5.
+        #. Use pop method with non valid data, the count of data shouldn't change.
+        #. Use pop method to pop certain index, the output shouldn't be 0.
+        #. Use count method to check that output, should equal to the length of tha original data -1.
         """
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
+        self.info("Use pop method with non valid data, the count of data shouldn't change")
+        self.client.pop(self.COLLECTION, self.BUCKET, 'post:15', "test")
 
         self.info("Use pop method to pop the latest index")
-        # self.assertNotEquals(self.client.pop(self.COLLECTION, self.BUCKET, 'post:5', "for the love lord?"), 0)
+        self.client.pop(self.COLLECTION, self.BUCKET, 'post:2', 'this is a hello {}'.format(self.RAND_STRING_2))
 
         self.info("Use count method to check that output")
-        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 5)
+        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), len(self.data)-1)
 
-    def test011_pop_for_non_exists_collection_and_bucket(self):
-        """
-        TC 533
-        Test Case to test pop method with non exists collection and bucket.
-
-        **Test scenario**
-        #. Use pop method to pop the latest index, the output should equal to 0.
-        """
-        self.info("Use pop method to pop the latest index")
-        self.assertEqual(self.client.pop("RANDOM_COLLECTION", "RANDOM_BUCKET", 'test', "test"), 0)
-
-    def test012_flush_for_exists_collection_and_bucket(self):
+    def test006_flush_for_certain_collection_and_bucket(self):
         """
         TC 534
-        Test Case to test flush for exists collection and bucket.
+        Test Case to flush for certain collection.
 
         **Test scenario**
-        #. Push the data to sonic server.
-        #. Use flush to flush certain collection with certain bucket.
-        #. Use count method to check that output, should equal to 0.
+        #. Use flush to flush certain collection.
+        #. Use count method to check that output, should be 0.
         """
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
 
         self.info("Use flush to flush certain collection with certain bucket")
-        # self.assertNotEquals(self.client.flush(self.COLLECTION, self.BUCKET), 0)
+        self.client.flush(self.COLLECTION)
 
         self.info("Use count method to check that output")
         self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 0)
 
-    def test013_flush_for_non_exists_collection_and_bucket(self):
-        """
-        TC 535
-        Test Case for flush method for non exists collection and method.
+        # self.info("Use flush to flush non existing collection with certain bucket")
+        # self.client.flush("RANDOM_COLLECTION", "RANDOM_BUCKET")
 
-        **Test scenario**
-        #. Use flush to flush non exists collection with certain bucket.
-        """
-        self.info("Use flush to flush non exists collection with certain bucket")
-        self.assertEquals(self.client.flush("RANDOM_COLLECTION", "RANDOM_BUCKET"), 0)
-
-    def test014_flush_object_method_for_exists_object_in_certain_collection_with_certain_bucket(self):
+    def test007_flush_object_method_for_certain_object_in_certain_collection_with_certain_bucket(self):
         """
         TC 536
         Test Case for flush_object method for certain object in certain collection with certain bucket.
 
         **Test scenario**
-        #. Push the data to sonic server.
         #. Use flush_object to flush certain object in collection with certain bucket.
         #. Use query to check that this record is flushed.
         """
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
-
         self.info("Use flush_object to flush certain object in collection with certain bucket")
-        self.assertNotEqual(self.client.flush_object(self.COLLECTION, self.BUCKET, 'post:4'), 0)
+        self.client.flush_object(self.COLLECTION, self.BUCKET, 'post:4')
 
         self.info("Use query to check that this record is flushed")
-        self.assertEqual(self.client.query(self.COLLECTION, self.BUCKET, "love"), ['post:5'])
+        self.assertEqual(self.client.query(self.COLLECTION, self.BUCKET, "love"), ['post:1'])
 
-    def test015_flush_object_method_for_non_exists_object_in_certain_collection_with_certain_bucket(self):
-        """
-        TC 537
-        Test Case for flush_object for non exists object.
+        # self.info("Use flush_object to flush non existing object in collection with certain bucket")
+        # self.client.flush_object("RANDOM_COLLECTION", "RANDOM_BUCKET", "RANDOM_OBJECT")
 
-        **Test scenario**
-        #. Use flush_object to flush non exists object in collection with certain bucket.
-        """
-        self.info("Use flush_object to flush non exists object in collection with certain bucket")
-        self.assertEqual(self.client.flush_object("RANDOM_COLLECTION", "RANDOM_BUCKET", "RANDOM_OBJECT"), 0)
-
-    def test016_flush_bucket_for_exists_bucket_in_certain_collection(self):
+    def test008_flush_bucket_for_certain_bucket_in_certain_collection(self):
         """
         TC 538
         Test Case for flush_object method for certain bucket in certain collection.
 
         **Test scenario**
-        #. Push the data to sonic server.
-        #. Push data to another bucket within the same collection.
+        #. use flush_bucket method to flush certain bucket.
         #. Check that their is no more objects in the second created bucket.
-
         """
-        self.info("Push the data to sonic server")
-        for article_id, content in self.data.items():
-            self.assertTrue(self.client.push(self.COLLECTION, self.BUCKET, article_id, content))
 
-        self.info("Push data to another bucket within the same collection")
-        self.assertTrue(self.client.push(self.COLLECTION, "posts_1", "test", "test for flush_bucket"))
+        self.info("use flush_bucket method to flush certain bucket")
+        self.client.flush_bucket(self.COLLECTION, self.BUCKET)
 
-        self.info("Check that there are no more objects in the second created bucket")
-        self.assertEqual(self.client.count(self.COLLECTION, "posts_1"), 0)
-
-        self.info("Check that there are objects in the first created bucket")
-        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 6)
-
-
-
-
-
-
-
-
-
-
+        self.info("Check count in {} bucket, should be 0")
+        self.assertEqual(self.client.count(self.COLLECTION, self.BUCKET), 0)
