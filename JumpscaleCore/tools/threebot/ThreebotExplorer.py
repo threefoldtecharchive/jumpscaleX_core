298f332dec30710adbfdb8bb64859ea9f529fcf6
serboctor <boctor.sarah@gmail.com>
2019-12-05 16:19:37 +0200

    Fix threebotclient actors list

--- JumpscaleCore/tools/threebot/ThreebotExplorer.py
+++ JumpscaleCore/tools/threebot/ThreebotExplorer.py
@@ -18,7 +18,7 @@ class ThreebotExplorer(j.baseclasses.object):
 
     @property
     def actors(self):
-        return self._client.actors_default
+        return self._client.actors_all
 
     def threebot_record_get(self, tid=None, name=None, die=True):
         """
