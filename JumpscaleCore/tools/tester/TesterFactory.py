62fb8a496edc35a3ecefab046c28f426d93ec7a7
BolaNasr <bola.nasr@live.com>
2019-12-17 13:09:35 +0200

    run all tests using one method

--- JumpscaleCore/tools/tester/TesterFactory.py
+++ JumpscaleCore/tools/tester/TesterFactory.py
@@ -5,34 +5,89 @@ from unittest import TestCase, skip
 
 from parameterized import parameterized
 
-locations = []
-location_search = "__jslocation_"
-for root, dirs, files in os.walk(j.core.tools.text_replace("{DIR_BASE}/code/github/threefoldtech")):
-    for file in files:
-        if file.endswith("Factory.py"):
-            file_path = os.path.join(root, file)
-            with open(file_path, "r") as f:
-                content = f.read()
-            if location_search in content:
-                jslocation = content.find(location_search)
-                location = content[content.find("=", jslocation) + 1 : content.find("\n", jslocation)]
-                locations.append(location.strip().strip("'").strip('"'))
+locations_libs = []
+locations_core = []
+locations_threebot = []
+
+
+class Tester:
+    def _get_all_tests(locations, path):
+
+        location_search = "__jslocation_"
+        for root, dirs, files in os.walk(j.core.tools.text_replace(path)):
+            for file in files:
+                if file.endswith("Factory.py"):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, "r") as f:
+                        content = f.read()
+                    if location_search in content:
+                        jslocation = content.find(location_search)
+                        location = content[content.find("=", jslocation) + 1 : content.find("\n", jslocation)]
+                        locations.append(location.strip().strip("'").strip('"'))
+
+
+Tester._get_all_tests(locations_libs, "{DIR_BASE}/code/github/threefoldtech/jumpscaleX_libs")
+Tester._get_all_tests(locations_core, "{DIR_BASE}/code/github/threefoldtech/jumpscaleX_core")
+Tester._get_all_tests(locations_threebot, "{DIR_BASE}/code/github/threefoldtech/jumpscaleX_threebot")
 
 
 class TesterFactory(j.baseclasses.factory_testtools):
     __jslocation__ = "j.tools.tester"
 
-    @parameterized.expand(locations)
-    def start(self, location):
+    @parameterized.expand(locations_core)
+    def test_core(self, location):
         module = eval(location)
         if "test" in dir(module):
-            if "install" in dir(module):
-                install = module.__getattribute__("install")
-                install()
             test = module.__getattribute__("test")
             test()
-        else:
-            self.skipTest(f"{location} doesn't have test")
 
-        print("TEST OK ALL PASSED")
+    @parameterized.expand(locations_libs)
+    def test_libs(self, location):
+        module = eval(location)
+        if "test" in dir(module):
+            test = module.__getattribute__("test")
+            test()
 
+    @parameterized.expand(locations_threebot)
+    def test_threebot(self, location):
+        module = eval(location)
+        if "test" in dir(module):
+            test = module.__getattribute__("test")
+            test()
+
+    def start(self):
+        j.servers.threebot.local_start_default(background=True)
+
+        try:
+            j.servers.myjobs.schedule(self._start_libs(), die=False)
+        except Exception as e:
+            raise j.exceptions.RuntimeError("problem in my jobs %s", str(e))
+        try:
+            j.servers.myjobs.schedule(self._start_core(), die=False)
+        except Exception as e:
+            raise j.exceptions.RuntimeError("problem in my jobs %s", str(e))
+        try:
+            j.servers.myjobs.schedule(self._start_threebot(), die=False)
+        except:
+            raise j.exceptions.RuntimeError("problem in my jobs %s", str(e))
+
+        print("ALL TEST OK")
+
+    def _start_libs(self):
+        for test in dir(j.tools.tester):
+            if test.startswith("test_libs_"):
+                test_start = self.__getattribute__(test)
+                test_start()
+
+    def _start_core(self):
+        jobs_objs = []
+        for test in dir(j.tools.tester):
+            if test.startswith("test_core_"):
+                test_start = self.__getattribute__(test)
+                test_start()
+
+    def _start_threebot(self):
+        for test in dir(j.tools.tester):
+            if test.startswith("test_threebot_"):
+                test_start = self.__getattribute__(test)
+                test_start()
