512a0d01c53ae4f83c62c6121a0eb12fcfc18da3
BolaNasr <bola.nasr@live.com>
2019-12-23 16:23:40 +0200

    run all tests of jumpscaleX

--- JumpscaleCore/tools/tester/TesterFactory.py
+++ JumpscaleCore/tools/tester/TesterFactory.py
@@ -57,31 +57,71 @@ class TesterFactory(j.baseclasses.factory_testtools):
 
     def start(self):
         j.servers.threebot.local_start_default(background=True)
+        core_tests = Core()
+        libs_tests = Libs()
+        threebot_tests = Threebot()
 
         try:
-            j.servers.myjobs.schedule(self._start_libs(), die=False)
-            j.servers.myjobs.schedule(self._start_core(), die=False)
-            j.servers.myjobs.schedule(self._start_threebot(), die=False)
+            # Tests of core
+            j.servers.myjobs.schedule(core_tests.clients)
+            j.servers.myjobs.schedule(core_tests.data)
+            j.servers.myjobs.schedule(core_tests.servers)
+            j.servers.myjobs.schedule(core_tests.tools)
+            j.servers.myjobs.schedule(core_tests.sal)
+            # Tests of libs
+            j.servers.myjobs.schedule(libs_tests.tests)
+            # Tests of threebot
+            j.servers.myjobs.schedule(threebot_tests.tests)
         except Exception as e:
             raise j.exceptions.RuntimeError("problem in my jobs %s", str(e))
 
         print("ALL TEST OK")
 
-    def _start_libs(self):
-        for test in dir(j.tools.tester):
-            if test.startswith("test_libs_"):
-                test_start = self.__getattribute__(test)
-                test_start()
 
-    def _start_core(self):
-        jobs_objs = []
-        for test in dir(j.tools.tester):
-            if test.startswith("test_core_"):
-                test_start = self.__getattribute__(test)
-                test_start()
-
-    def _start_threebot(self):
+class Threebot:
+    def tests():
         for test in dir(j.tools.tester):
             if test.startswith("test_threebot_"):
-                test_start = self.__getattribute__(test)
+                test_start = j.tools.tester.__getattribute__(test)
+                test_start()
+
+
+class Core:
+    def clients():
+
+        core_clients = [x for x in dir(j.tools.tester) if "core_" in x and "clients" in x]
+        for test in core_clients:
+            test_start = j.tools.tester.__getattribute__(test)
+            test_start()
+
+    def data():
+        core_data = [x for x in dir(j.tools.tester) if "core_" in x and "data" in x]
+        for test in core_data:
+            test_start = j.tools.tester.__getattribute__(test)
+            test_start()
+
+    def servers():
+        core_servers = [x for x in dir(j.tools.tester) if "core_" in x and "servers" in x]
+        for test in core_servers:
+            test_start = j.tools.tester.__getattribute__(test)
+            test_start()
+
+    def tools():
+        core_tools = [x for x in dir(j.tools.tester) if "core_" in x and "tools" in x]
+        for test in core_tools:
+            test_start = j.tools.tester.__getattribute__(test)
+            test_start()
+
+    def sal():
+        core_sal = [x for x in dir(j.tools.tester) if "core_" in x and "sal" in x]
+        for test in core_sal:
+            test_start = j.tools.tester.__getattribute__(test)
+            test_start()
+
+
+class Libs:
+    def tests():
+        for test in dir(j.tools.tester):
+            if test.startswith("test_libs_"):
+                test_start = j.tools.tester.__getattribute__(test)
                 test_start()
