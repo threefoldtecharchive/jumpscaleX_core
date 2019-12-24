1103748f1d145cde8c0ef1adc00b3023424df44c
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-12-24 12:45:18 +0200

    add how to reload a package wiki in quickstart.md

--- JumpscaleCore/servers/openresty/OpenRestyServer.py
+++ JumpscaleCore/servers/openresty/OpenRestyServer.py
@@ -41,7 +41,7 @@ class OpenRestyServer(j.baseclasses.factory_data):
         j.sal.fs.createDir(self.path_web)
         j.sal.fs.createDir(self.path_cfg_dir)
         # clean old websites config
-        j.sal.fs.remove("%s/servers" % self.path_cfg_dir)
+        self.cleanup()
         self.executor = "tmux"  # only tmux for now
 
         self.install()
@@ -187,3 +187,6 @@ class OpenRestyServer(j.baseclasses.factory_data):
         self.configure()
         cmd = "cd  %s;lapis build" % self.path_cfg_dir
         j.sal.process.execute(cmd)
+
+    def cleanup(self):
+        j.sal.fs.remove("%s/servers" % self.path_cfg_dir)
