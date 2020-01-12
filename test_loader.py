961fc89f50b1ace9fb3ccd8824999e77f2ed3a1f
ahmedhanafy725 <hanafya@codescalers.com>
2020-01-12 13:59:53 +0200

    Add more jsx tests to run on zeroCI

--- test_loader.py
+++ test_loader.py
@@ -33,20 +33,44 @@ class CoreTests(TestCase):
     @parameterized.expand(
         [
             "j.data.bcdb.test()",
-            "j.clients.zdb.test()",
+            "j.data.capnp.test()",
+            "j.data.nacl.test()",
             "j.data.schema.test()",
+            "j.data.types.test()",
             "j.clients.sshkey.test()",
             "j.clients.sshagent.test()",
+            "j.clients.ssh.test()",
+            "j.sal.bcdbfs.test()",
+            "j.tools.threebot.packages.test()",
+            "j.tools.syncer.test()",
+            "j.tools.executor.test()",
+            "j.tools.time.test()",
+            "j.tools.formatters.test()",
+            "j.tools.threegit.test()" "j.tools.logger.test()",
+            "j.tools.jinja2.test()",
+            "j.tools.restic.test()",
+            "j.clients.redis.test()",
+            "j.clients.tfgrid_registry.test()",
+            "j.clients.sqlitedb.test()",
+            "j.clients.currencylayer.test()",
+            "j.clients.sonic.test()",
+            "j.clients.rdb.test()",
+            "j.clients.redis_config.test()",
+            "j.clients.threebot.test()",
+            "j.clients.tcp_router.test()",
+            "j.clients.zdb.test()",
+            "j.servers.gedis.test()",
+            "j.servers.threebot.test()",
             "j.servers.openresty.test()",
             "j.servers.myjobs.test()",
+            "j.servers.sonic.test()",
+            "j.servers.corex.test()",
+            "j.servers.tmux.test()",
+            "j.servers.zdb.test()",
         ]
     )
     def test(self, cmd):
         if cmd == "j.clients.zdb.test()":
             self.skipTest("This test shouldn't be run on CI, as it depends on machine speed (performance test)")
-        elif cmd == "j.servers.myjobs.test()":
-            self.skipTest("https://github.com/threefoldtech/jumpscaleX_core/issues/342")
-        elif cmd == "j.servers.openresty.test()":
-            self.skipTest("https://github.com/threefoldtech/jumpscaleX_core/issues/364")
         else:
             eval(cmd)
