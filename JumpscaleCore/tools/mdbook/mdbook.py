28b4042dcc484cc665fec8f1c5db9c3fc365fc02
AhmedSa-mir <asamir97@hotmail.com>
2020-04-09 16:27:41 +0000

    Implemented 2 methods for package books generation: generate_pkg_summary() - generate_summaries()

--- JumpscaleCore/tools/mdbook/mdbook.py
+++ JumpscaleCore/tools/mdbook/mdbook.py
@@ -29,3 +29,56 @@ class MdBook(j.baseclasses.object):
 
     def get_book_path(self, name):
         return j.sal.fs.joinPaths(self.output_path, name)
+
+    def generate_summaries(self):
+        """ Generate summaries for all packages """
+        packages = j.tools.threebot_packages.find()
+        for package in packages:
+            pkg_wiki_path = package.path + 'wiki/'
+            if j.sal.fs.exists(path=pkg_wiki_path):
+                self.generate_pkg_summary(name=package.name)
+    
+    def generate_pkg_summary(self, name):
+
+        #TODO: check failed links to readme files (readme-> index in mdbook build)
+        #TODO: index.html in wiki (error)
+
+        self.summary = ""
+        pkg_path = ""
+        if not j.tools.threebot_packages.find(name=name):
+            raise ValueError(f"Package {name} does not exist")
+        else:
+            pkg_path = j.tools.threebot_packages.find(name=name)[0].path
+
+        # Remove old summary and create README if not exists
+        if j.sal.fs.exists(path=f"{pkg_path}/wiki/SUMMARY.md"):
+            j.sal.fs.remove(path=f"{pkg_path}/wiki/SUMMARY.md")
+        if not j.sal.fs.exists(path=f"{pkg_path}/wiki/README.md"):
+            j.sal.fs.writeFile(filename=pkg_path+'wiki/README.md', contents=name)
+        
+        # Recursive generation of summary from md files
+        def dir_summary(path, indent):
+            """
+            @param path: path of the dir
+            @param indent: lvl of indentation of the current dir in the summary file
+            """
+            md_files = j.sal.fs.listFilesInDir(path=path, filter="*.md", depth=0)
+            for file in md_files:
+                rel_path = file.replace(pkg_path+'wiki/','') # remove pkg/wiki/
+                filename = j.sal.fs.getBaseName(file)   # file.md
+                if filename == 'README.md':
+                    if rel_path == 'README.md': # readme in wiki dir
+                        link_name = j.sal.fs.getDirName(path, levelsUp=0)
+                    else:
+                        link_name = j.sal.fs.getDirName(file, levelsUp=0)
+                    self.summary += f"- [{link_name}]({rel_path})\n"
+                else:
+                    self.summary += f"{indent}- [{filename.rsplit('.', 1)[0]}]({rel_path})\n"
+            
+            dirs = j.sal.fs.listDirsInDir(path=path)
+            for dir in dirs:
+                dir_summary(dir, indent + '  ')
+
+        dir_summary(path=pkg_path+'wiki', indent='')
+        
+        j.sal.fs.writeFile(filename=pkg_path+'wiki/SUMMARY.md', contents=self.summary)
