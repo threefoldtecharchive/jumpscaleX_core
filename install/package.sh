834da0ddd32f891e660b5f9238067c8d3b7b4666
rkhamis <reem.khamis@gmail.com>
2020-04-14 12:30:07 +0200

    uncomment and restructure building on linux

--- install/package.sh
+++ install/package.sh
@@ -2,18 +2,19 @@
 set -e
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     pip3 install staticx
+    pip3 uninstall pyinstaller
+    pip3 install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
+    if [ "$(whoami)" == "root" ]; then
+        pip3 install -r requirements.txt
+    else
+        pip3 install --user -r requirements.txt
+    fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
     brew install upx
 fi
 
-#pip3 uninstall pyinstaller
-#pip3 install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
-#if [ "$(whoami)" == "root" ]; then
-#    pip3 install -r requirements.txt
-#else
-#    pip3 install --user -r requirements.txt
-#fi
+
 cp InstallTools.py sdk/
 pyinstaller 3sdk.spec
 rm sdk/InstallTools.py
