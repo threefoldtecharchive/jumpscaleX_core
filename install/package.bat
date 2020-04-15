7f9a5be7b534423b14532b4d1d5b9e532f95e709
waleed <waleed.hammam@gmail.com>
2020-04-15 18:21:42 +0200

    update package.bat

--- install/package.bat
+++ install/package.bat
@@ -3,8 +3,9 @@
 title JumpscaleX SDK windows packager
 echo Creating binary
 
-pip3 install -r requirements.txt
-pip3 install staticx
+pip install -r requirements.txt
+pip install staticx
+pip install pypiwin32
 
 cp InstallTools.py sdk/
 pyinstaller 3sdk.spec
