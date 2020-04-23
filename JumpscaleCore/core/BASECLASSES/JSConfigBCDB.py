6c6a7ce10cab4ca78a0d4d7abc0fc043f6d551fd
Mahmoud Ayoub <mahmoud.a@incubaid.com>
2020-04-23 07:54:57 +0200

    Revert "fix delete instances of object"
    This reverts commit 7be498c6fb93456da2ecb3c1059743c18007381c.

--- JumpscaleCore/core/BASECLASSES/JSConfigBCDB.py
+++ JumpscaleCore/core/BASECLASSES/JSConfigBCDB.py
@@ -142,8 +142,6 @@ class JSConfigBCDB(JSConfigBCDBBase):
                     del self._parent._children[self._data.name]
         assert self._data.name
         self._children_delete()
-        model = self._model.get(self.id)
-        model.delete()
 
     def save(self):
         self.save_()
