cbf175f2d01982c53c680fff1feed54ff1a16a2f
Jo De Boeck <deboeck.jo@gmail.com>
2020-04-26 12:51:32 +0200

    Harden windows building
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- install/package_windows.sh
+++ install/package_windows.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -e
+if ! which wine > /dev/null; then
+    echo "Please install wine and try again"
+    exit 1
+fi
 export WINEPREFIX="$(pwd)/windows"
 mkdir -p "${WINEPREFIX}"
 PYTHONURL="https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe"
