5bfefeaedc4b09f25ebb6c6affad76b4780e9cfa
Waleed <waleed.hammam@gmail.com>
2020-05-17 19:35:10 +0200

    add nsis building for windows (#849)
    * add nsis building for windows
    
    * automate package.bat to use nsis
    
    * update building windows script

--- install/package.bat
+++ install/package.bat
@@ -9,5 +9,11 @@ pip install pypiwin32
 
 pyinstaller 3sdk.spec
 
+REM Create NSIS
+powershell.exe -command "& Invoke-WebRequest https://netix.dl.sourceforge.net/project/nsis/NSIS%203/3.05/nsis-3.05-setup.exe -O nsis-3.05-setup.exe"
+C:\Users\%USERNAME%\nsis-3.05-setup.exe /S
+SET PATH=%PATH%;C:\Program Files (x86)\NSIS
+makensis 3sdk_nsis.nsi
+
 echo Done! Press any key to continue ...
 pause
