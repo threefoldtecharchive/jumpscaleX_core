096d634ee470cf9428bacab6059ccd4660eaad87
Jo De Boeck <deboeck.jo@gmail.com>
2020-05-17 17:18:09 +0300

    Improve status reporting during update/clone (#850)
    See: https://github.com/threefoldtech/home/issues/716
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- install/threesdk/InstallTools.py
+++ install/threesdk/InstallTools.py
@@ -32,7 +32,7 @@ import requests
 
 from pathlib import Path
 from subprocess import Popen
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 import inspect
 import json
 
@@ -67,75 +67,63 @@ RepoInfo = namedtuple("RepoInfo", ["protocol", "host", "account", "name", "url",
 GITHUB_RSA = "AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 
 DEFAULT_BRANCH_WEB = "development"
-GITREPOS = {}
+GITREPOS = OrderedDict()
 
-GITREPOS["builders_extra"] = [
-    "https://github.com/threefoldtech/jumpscaleX_builders",
-    "%s" % DEFAULT_BRANCH,
-    "JumpscaleBuildersExtra",
-    "{DIR_BASE}/lib/jumpscale/JumpscaleBuildersExtra",
-]
-GITREPOS["installer"] = [
-    "https://github.com/threefoldtech/jumpscaleX_core",
-    "%s" % DEFAULT_BRANCH,
-    "install",  # directory in the git repo
-    "{DIR_BASE}/installer",
-]
 GITREPOS["core"] = [
     "https://github.com/threefoldtech/jumpscaleX_core",
-    "%s" % DEFAULT_BRANCH,
+    DEFAULT_BRANCH,
     "JumpscaleCore",
     "{DIR_BASE}/lib/jumpscale/Jumpscale",
 ]
-GITREPOS["home"] = ["https://github.com/threefoldtech/home", "master", "", "{DIR_BASE}/lib/jumpscale/home"]
-
+GITREPOS["installer"] = [
+    "https://github.com/threefoldtech/jumpscaleX_core",
+    DEFAULT_BRANCH,
+    "install",  # directory in the git repo
+    "{DIR_BASE}/installer",
+]
 GITREPOS["builders"] = [
     "https://github.com/threefoldtech/jumpscaleX_builders",
-    "%s" % DEFAULT_BRANCH,
+    DEFAULT_BRANCH,
     "JumpscaleBuilders",
     "{DIR_BASE}/lib/jumpscale/JumpscaleBuilders",
 ]
-
-GITREPOS["builders_community"] = [
+GITREPOS["builders_extra"] = [
     "https://github.com/threefoldtech/jumpscaleX_builders",
-    "%s" % DEFAULT_BRANCH,
+    DEFAULT_BRANCH,
+    "JumpscaleBuildersExtra",
+    "{DIR_BASE}/lib/jumpscale/JumpscaleBuildersExtra",
+]
+GITREPOS["builders_builders_community"] = [
+    "https://github.com/threefoldtech/jumpscaleX_builders",
+    DEFAULT_BRANCH,
     "JumpscaleBuildersCommunity",
     "{DIR_BASE}/lib/jumpscale/JumpscaleBuildersCommunity",
 ]
-
-GITREPOS["libs_extra"] = [
-    "https://github.com/threefoldtech/jumpscaleX_libs_extra",
-    "%s" % DEFAULT_BRANCH,
-    "JumpscaleLibsExtra",
-    "{DIR_BASE}/lib/jumpscale/JumpscaleLibsExtra",
-]
+GITREPOS["home"] = ["https://github.com/threefoldtech/home", "master", "", "{DIR_BASE}/lib/jumpscale/home"]
 GITREPOS["libs"] = [
     "https://github.com/threefoldtech/jumpscaleX_libs",
-    "%s" % DEFAULT_BRANCH,
+    DEFAULT_BRANCH,
     "JumpscaleLibs",
     "{DIR_BASE}/lib/jumpscale/JumpscaleLibs",
 ]
+GITREPOS["libs_extra"] = [
+    "https://github.com/threefoldtech/jumpscaleX_libs_extra",
+    DEFAULT_BRANCH,
+    "JumpscaleLibsExtra",
+    "{DIR_BASE}/lib/jumpscale/JumpscaleLibsExtra",
+]
 GITREPOS["threebot"] = [
     "https://github.com/threefoldtech/jumpscaleX_threebot",
-    "%s" % DEFAULT_BRANCH,
+    DEFAULT_BRANCH,
     "ThreeBotPackages",
     "{DIR_BASE}/lib/jumpscale/threebot_packages",
 ]
-
-GITREPOS["tutorials"] = [
-    "https://github.com/threefoldtech/jumpscaleX_libs",
-    "%s" % DEFAULT_BRANCH,
-    "tutorials",
-    "{DIR_BASE}/lib/jumpscale/tutorials",
-]
-
-GITREPOS["tutorials"] = [
+GITREPOS["weblibs"] = [
     "https://github.com/threefoldtech/jumpscaleX_weblibs",
     "%s" % DEFAULT_BRANCH_WEB,
     "static",
     "{DIR_BASE}/lib/weblibs/static",
 ]
-
 GITREPOS["kosmos"] = [
     "https://github.com/threefoldtech/jumpscaleX_threebot",
     "%s" % DEFAULT_BRANCH,
@@ -4994,32 +4982,36 @@ class JumpscaleInstaller:
         if prebuilt:
             GITREPOS["prebuilt"] = PREBUILT_REPO
 
-        done = []
         usessh = False if executor else Tools.code_github_use_ssh()
         execute = executor.execute if executor else Tools.execute
 
-        for NAME, d in GITREPOS.items():
-            GITURL, BRANCH, RPATH, DEST = d
-            if GITURL in done:
-                continue
+        repos = []
+        foundurls = set()
+        for NAME, repoinfo in GITREPOS.items():
+            GITURL, BRANCH, RPATH, DEST = repoinfo
+            if GITURL not in foundurls:
+                repos.append({"name": NAME, "url": GITURL, "branch": BRANCH})
+                foundurls.add(GITURL)
 
+        count = len(repos)
+        for idx, repo in enumerate(repos):
+            print(f"Updating repo {repo['name']:<20} {idx+1}/{count}", end="\r")
             if usessh:
-                GITURL = Tools.code_git_rewrite_url(GITURL, ssh=True).url
+                repo["url"] = Tools.code_git_rewrite_url(repo["url"], ssh=True).url
 
             if branch:
                 # check if provided branch exists otherwise don't use it
-                C = f"""git ls-remote --heads {GITURL} {branch} {GITURL}"""
+                C = f"""git ls-remote --heads {repo['url']} {branch} {repo['url']}"""
                 _, out, _ = execute(C, showout=False, interactive=False)
                 if out:
-                    BRANCH = branch
+                    repo["branch"] = branch
 
             try:
-                Tools.code_github_get(url=GITURL, rpath=RPATH, branch=BRANCH, pull=pull, reset=reset, executor=executor)
+                Tools.code_github_get(url=repo["url"], branch=repo["branch"], pull=pull, reset=reset, executor=executor)
             except Tools.exceptions.Input:
                 raise
 
-            done.append(GITURL)
-
+        print("")
         if prebuilt:
             self.prebuilt_copy()
 
