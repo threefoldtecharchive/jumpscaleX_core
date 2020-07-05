2e056cdb301e7ecb08fa6c0c8d3c726acf92c1da
rkhamis <reem.khamis@gmail.com>
2020-07-05 11:54:30 +0200

    bump version

--- install/threesdk/__init__.py
+++ install/threesdk/__init__.py
@@ -7,7 +7,7 @@ import textwrap
 # import jsx
 
 __all__ = ["builder", "simulator", "container", "args", "core", "installer", "install", "threebot"]
-__version__ = "v10.6"
+__version__ = "v10.7"
 
 IT = core.core.IT
 
